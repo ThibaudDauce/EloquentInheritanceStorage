eb33ec746972962c82ae51e2aaa26516a53033a0
Thibaud Dauce <thibaud@dauce.fr>
2014-07-27 17:53:03 +0200

    Override save and getTable function

--- src/ThibaudDauce/EloquentInheritanceStorage/ParentTrait.php
+++ src/ThibaudDauce/EloquentInheritanceStorage/ParentTrait.php
@@ -1,5 +1,44 @@
 <?php namespace ThibaudDauce\EloquentInheritanceStorage;
 
+use ThibaudDauce\EloquentInheritanceStorage\Facades\InheritanceStorage as InheritanceStorageFacade;
+use ThibaudDauce\EloquentInheritanceStorage\InheritanceStorage as InheritanceStorage;
+
 trait ParentTrait {
 
+  // Default value : get models from view
+  private $inheritanceStorageMode = InheritanceStorage::VIEW_MODE;
+
+  // Default view name : "models_storage"
+  private $storageSuffix = 'storage';
+  private $storageSeparator = '_';
+
+  /**
+   * Save the model to the database.
+   *
+   * @override Illuminate\Database\Eloquent\Model
+   * @param  array  $options
+   * @return bool
+   */
+  public function save(array $options = array())
+  {
+    $previousMode = $this->setMode(Inheritance::SET_MODE);
+
+    parent::save($options);
+
+    $this->setMode($previousMode);
+  }
+
+  /**
+   * Get the table or view associated with the model.
+   *
+   * @override Illuminate\Database\Eloquent\Model
+   * @return string
+   */
+  public function getTable()
+  {
+    if ($this->getMode() == Inheritance::GET_MODE)
+      return parent::getTable();
+    else
+      return $this->getStorage();
+  }
 }
