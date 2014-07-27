9c3fc4ed00a0f3980c691fd3a67fd2d2e703dd8d
Thibaud Dauce <thibaud@dauce.fr>
2014-07-27 17:48:33 +0200

    Add getters for constants

--- src/ThibaudDauce/EloquentInheritanceStorage/InheritanceStorage.php
+++ src/ThibaudDauce/EloquentInheritanceStorage/InheritanceStorage.php
@@ -6,8 +6,19 @@ class InheritanceStorage {
    * true : switch between view and storage table ON
    * false : switch between view and storage table OFF, only return storage table.
   **/
-  protected $activated = true;
+  public $activated = true;
 
   const VIEW_MODE    = 'view';
   const STORAGE_MODE = 'storage';
+
+  /*
+   * Getters for const values 
+  **/
+  public function VIEW_MODE() {
+    return static::VIEW_MODE;
+  }
+
+  public function STORAGE_MODE() {
+    return static::STORAGE_MODE;
+  }
 }
